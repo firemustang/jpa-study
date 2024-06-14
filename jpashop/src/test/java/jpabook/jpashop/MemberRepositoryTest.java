f146c5400a1cab1ca208ede32c905ab95f3d082d
firemustang <jeongeuuun@gmail.com>
2024-06-14 19:25:57 +0900

    엔티티 설계

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -12,21 +12,21 @@ import org.springframework.transaction.annotation.Transactional;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MemberRepositoryTest {
-    @Autowired MemberRepository memberRepository;
-    
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    public void testMember() throws Exception {
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        Long save = memberRepository.save(member);
-        Member findMember = memberRepository.find(save);
-
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
-        System.out.println("findMember == member :" + (findMember == member));
-    }
+//    @Autowired MemberRepositoryTest memberRepository;
+//
+//    @Test
+//    @Transactional
+//    @Rollback(value = false)
+//    public void testMember() throws Exception {
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        Long save = memberRepository.save(member);
+//        Member findMember = memberRepository.find(save);
+//
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//        System.out.println("findMember == member :" + (findMember == member));
+//    }
 }
