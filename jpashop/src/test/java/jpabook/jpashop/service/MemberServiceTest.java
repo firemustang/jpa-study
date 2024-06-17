3200943bec2dff9073ab1c479d7f96d69e15091b
firemustang <jeongeuuun@gmail.com>
2024-06-17 22:25:34 +0900

    order 로직 추가

--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -21,6 +21,7 @@ class MemberServiceTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Rollback(value = false)
     public void 회원가입() throws Exception {
         //given
 
@@ -37,9 +38,21 @@ class MemberServiceTest {
     @Test
     public void 중복_회원_예외() throws Exception {
         //given
-        
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
         //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2); //예외가 발생해야함
+        } catch (IllegalStateException e){
+            return;
+        }
         //then
+        fail("예외가 발생해야함");
+
     }
 
 }
